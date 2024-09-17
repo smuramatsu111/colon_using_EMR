d6963d48d7264263320689c4e33652018ef02d68
smuramatsu111 <85828346+smuramatsu111@users.noreply.github.com>
2024-09-17 16:06:00 +0900

    Create logger_time.py

---
+++ ssmixparser/mylib/logger_time.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import sys
+import time
+from contextlib import contextmanager
+
+import logging
+
+
+LOGGER = logging.getLogger()
+FORMATTER = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+
+
+def setup_logger(out_file=None, stderr=True, stderr_level=logging.INFO, file_level=logging.DEBUG):
+    LOGGER.handlers = []
+    LOGGER.setLevel(min(stderr_level, file_level))
+
+    if stderr:
+        handler = logging.StreamHandler(sys.stderr)
+        handler.setFormatter(FORMATTER)
+        handler.setLevel(stderr_level)
+        LOGGER.addHandler(handler)
+
+    if out_file is not None:
+        handler = logging.FileHandler(out_file)
+        handler.setFormatter(FORMATTER)
+        handler.setLevel(file_level)
+        LOGGER.addHandler(handler)
+
+    LOGGER.info("logger set up")
+    return LOGGER
+
+
+@contextmanager
+def timer(name):
+    t0 = time.time()
+    yield
+    LOGGER.info(f'[{name}] done in {time.time() - t0:.0f} s')
